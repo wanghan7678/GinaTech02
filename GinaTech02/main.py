ded8a03d300e3d02d89e5d3f6739fdfef3b9f7fa
Han Wang <han.wang@greenrich.dk>
2019-08-01 23:34:53 +0200

    add apscheduler to the system add exception

--- GinaTech02/main.py
+++ GinaTech02/main.py
@@ -21,17 +21,16 @@ def schedulTask():
     scheduler.add_job(us_predict_today, 'cron', hour=4, minute=10)
     scheduler.add_job(cn_add_today, 'cron', hour=11, minute=10)
     scheduler.add_job(cn_predict_today, 'cron', hour=12, minute=10)
-    scheduler.start()
-
+    try:
+        scheduler.start()
+    except(KeyboardInterrupt, SystemExit):
+        pass
 
 
 if __name__ == '__main__':
 
     print("start program...")
 
-    #serv.ann_predict_cn('cn_2019-07-25_weight.h5')
-    #serv.ann_predict_us('us_2019-07-14_weight.h5')
-    #serv.insert_alldaily_oneday_us('2019-07-25')
-    serv.insert_alldaily_oneday_cn('2019-07-29')
+    schedulTask()
 
     print("end program.")
